b903bd490db63cffd5dafc51f0d7d30b7beda11b
Manny Rodriguez <mannyrodriguez011@gmail.com>
2024-02-10 00:27:32 +0530

    added some styling to the footer;

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,13 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
+import '../styles/Footer.css';
+
 export default function Footer() {
   return (
-    <div>
+    <div className='footer'>
         &copy; Manny53365 {new Date().getFullYear()}
-        <a href="https://github.com/manny53365"><FontAwesomeIcon icon={faGithub} target='_blank' rel='noopener noreferrer' /></a>
+        <a className='gh-link' href="https://github.com/manny53365"><FontAwesomeIcon icon={faGithub} target='_blank' rel='noopener noreferrer' /></a>
     </div>
   )
 }
