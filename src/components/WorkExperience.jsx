b9488164551be8ef00dafbac9ce53d53c6b1d14d
Manny Rodriguez <mannyrodriguez011@gmail.com>
2024-02-09 23:10:19 +0530

    added starter code for the inputs with placeholders

--- src/components/WorkExperience.jsx
+++ src/components/WorkExperience.jsx
@@ -4,6 +4,13 @@ export default function WorkExperience() {
   return (
     <div>
         <h2>Work Experience</h2>
+        <input type="text" placeholder='Title/Role'/>
+        <input type="text" placeholder='Company Name'/>
+        <input type="text" placeholder='City'/>
+        <input type="text" placeholder='From Date'/>
+        <input type="text" placeholder='To Date'/>
+        <button>Delete</button>
+        <button>Add</button>
     </div>
   )
-};
+}
