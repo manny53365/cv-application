661b60007ee4b308d398dd553d08d101d0622849
Manny Rodriguez <mannyrodriguez011@gmail.com>
2024-02-09 22:57:18 +0530

    added some starter state for the first and last name fields

--- src/components/PersonalInfo.jsx
+++ src/components/PersonalInfo.jsx
@@ -8,10 +8,12 @@ export default function PersonalInfo() {
   return (
     <div className='personal-info'>
         <h2>Personal Information</h2>
-        <input type="text" placeholder='First Name' />
-        <input type="text" placeholder='Last Name'/>
+        <input type="text" placeholder='First Name' onChange={event => setFName(event.target.value)} value={fName}/>
+        <input type="text" placeholder='Last Name' onChange={event => setLName(event.target.value)} value={lName}/>
         <input type="text" placeholder='Title'/>
-        <input type="file" placeholder='Photo'/>
+        <label>
+            <input type="file" name='Photo'/>Photo
+        </label>
         <input type="text" placeholder='Address' />
         <input type="text" placeholder='Phone Number'/>
         <input type="text" placeholder='Email'/>
