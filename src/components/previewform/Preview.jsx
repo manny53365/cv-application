ec3dd45fcfbd102cf38cbf996d22a0d8198b6ec4
Manny Rodriguez <mannyrodriguez011@gmail.com>
2024-02-13 14:56:40 +0530

    added updates to styles to make the preview output responsive

--- src/components/previewform/Preview.jsx
+++ src/components/previewform/Preview.jsx
@@ -6,12 +6,19 @@ export default function Preview({ cv }) {
     <div className="preview-container">
         Preview Page: {JSON.stringify(cv)}
         <div className="personal-info-preview">
-            <h1 className='preview-name'>{cv.personalInfo.fName} {cv.personalInfo.lName}</h1>
-            <h4 className='personal-info-title'>{cv.personalInfo.title}</h4>
+            <div className="header-container">
+                <div className="name-and-title">
+                    <h1 className='preview-name'>{cv.personalInfo.fName} {cv.personalInfo.lName}</h1>
+                    <h4 className='personal-info-title'>{cv.personalInfo.title}</h4>
+                </div>
+                <div className="contact-info">
+                    <p className='preview-address'>{cv.personalInfo.address}</p>
+                    <p className='preview-phone'>{cv.personalInfo.phone}</p>
+                    <a className='preview-email' href={`mailto:${cv.personalInfo.email}`}>{cv.personalInfo.email}</a>
+                </div>
+            </div>
+            <hr />
             {cv.personalInfo.photo && <img className='preview-photo' src={cv.personalInfo.photo} alt='user' />}
-            <p className='preview-address'>{cv.personalInfo.address}</p>
-            <p className='preview-phone'>{cv.personalInfo.phone}</p>
-            <a className='preview-email' href={`mailto:${cv.personalInfo.email}`}>{cv.personalInfo.email}</a>
             <p className='preview-intro'>{cv.personalInfo.intro}</p>
         </div>
     </div>
